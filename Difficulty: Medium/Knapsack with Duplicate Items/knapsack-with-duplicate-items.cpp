ee07846b10f4cf94c7dbf5a79d4afbc415ed5d5b
21A91A04O5 <98264977+21A91A04O5@users.noreply.github.com>
2024-08-11 13:40:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -9,21 +9,31 @@ using namespace std;
 
 class Solution{
 public:
+    int knap(int wt[],int val[],int W,int n,vector<vector<int>>& dp){
+     if(W==0 or n==0) return 0;
+     if(dp[n][W]!=-1) return dp[n][W];
+     if(wt[n-1]<=W)
+         return dp[n][W]=max(val[n-1]+knap(wt,val,W-wt[n-1],n,dp),knap(wt,val,W,n-1,dp));
+     else
+         return dp[n][W]=knap(wt,val,W,n-1,dp);
+    }
     int knapSack(int N, int W, int val[], int wt[])
     {
-        vector<vector<int>> dp(N+1,vector<int>(W+1));
-        for(int i=0;i<=N;i++){
-            for(int j=0;j<=W;j++){
-                if(i==0 || j==0) dp[i][j]=0;
-                else{
-                    if(wt[i-1]<=j){
-                        dp[i][j]=max(val[i-1]+dp[i][j-wt[i-1]],dp[i-1][j]);
-                    }
-                    else dp[i][j]=dp[i-1][j];
-                }
-            }
-        }
-        return dp[N][W];
+        vector<vector<int>> dp(N+1,vector<int>(W+1,-1));
+        return knap(wt,val,W,N,dp);
+        // vector<vector<int>> dp(N+1,vector<int>(W+1));
+        // for(int i=0;i<=N;i++){
+        //     for(int j=0;j<=W;j++){
+        //         if(i==0 or j==0) dp[i][j]=0;
+        //         else{
+        //             if(wt[i-1]<=j){
+        //                 dp[i][j]=max(val[i-1]+dp[i][j-wt[i-1]],dp[i-1][j]);
+        //             }
+        //             else dp[i][j]=dp[i-1][j];
+        //         }
+        //     }
+        // }
+        // return dp[N][W];
     }
 };
 
