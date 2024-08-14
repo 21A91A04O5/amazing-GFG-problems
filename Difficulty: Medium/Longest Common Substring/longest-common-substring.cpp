109e3364245471016e7c27081ee5a2e083583cd9
21A91A04O5 <98264977+21A91A04O5@users.noreply.github.com>
2024-08-14 07:08:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -7,11 +7,11 @@ using namespace std;
 class Solution {
   public:
     int longestCommonSubstr(string str1, string str2) {
-        int m=str1.size(),n=str2.size(),ans=0;
-        vector<vector<int>> dp(m+1,vector<int>(n+1));
-        for(int i=0;i<m+1;i++){
-            for(int j=0;j<n+1;j++){
-                if(i==0 or j==0) dp[i][j]=0;
+        int a=str1.size(),b=str2.size(),ans=0;
+        vector<vector<int>> dp(a+1,vector<int>(b+1));
+        for(int i=0;i<a+1;i++){
+            for(int j=0;j<b+1;j++){
+                if(i==0 and j==0) dp[i][j]=0;
                 else{
                     if(str1[i-1]==str2[j-1]){
                         dp[i][j]=1+dp[i-1][j-1];
