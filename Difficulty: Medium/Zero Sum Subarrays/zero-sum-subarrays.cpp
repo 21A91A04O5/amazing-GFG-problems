2125bb2909c2cac4871208fe1df68cf319442b4b
21A91A04O5 <98264977+21A91A04O5@users.noreply.github.com>
2025-01-03 00:10:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Zero Sum Subarrays/zero-sum-subarrays.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findSubarray(vector<int> &arr) {
+        // code here.
+        int sum=0,ans=0;
+        map<int,int> mpp;
+        mpp[0]=1;
+        for(int i=0;i<arr.size();i++){
+            sum+=arr[i];
+            ans+=mpp[sum];
+            mpp[sum]++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        vector<int> arr;
+        while (ss >> num)
+            arr.push_back(num);
+
+        Solution ob;
+        cout << ob.findSubarray(arr) << endl;
+        cout << "~\n";
+    }
+}
+
+// } Driver Code Ends
